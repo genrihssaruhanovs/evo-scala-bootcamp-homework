20817bd74b29243e3e48d0649c5e810b53516667
genrihssaruhanovs <genrihs.saruhanovs@gmail.com>
2020-09-23 12:10:08 +0300

    minor code reformatting

--- src/main/scala/evo/homework/Main.scala
+++ src/main/scala/evo/homework/Main.scala
@@ -2,11 +2,11 @@ package evo.homework
 
 import scala.annotation.tailrec
 
-object Main{
+object Main {
   def lcm(a: Int, b: Int): Int =
-    if( a == b && a == 0 ) 0 else Math.abs(a * b) / gcd(a, b)
+    if (a == b && a == 0) 0 else Math.abs(a * b) / gcd(a, b)
 
   @tailrec
   def gcd(a: Int, b: Int): Int =
-    if ( b == 0 ) a else gcd(b, a % b)
+    if (b == 0) a else gcd(b, a % b)
 }
