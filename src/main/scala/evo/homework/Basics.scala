7c1e1b77fb95b5158c9469df675a666a832db14d
genrihssaruhanovs <genrihs.saruhanovs@gmail.com>
2020-09-24 21:28:48 +0300

    Class renaming

--- src/main/scala/evo/homework/Basics.scala
+++ src/main/scala/evo/homework/Basics.scala
@@ -2,7 +2,7 @@ package evo.homework
 
 import scala.annotation.tailrec
 
-object Main {
+object Basics {
   def lcm(a: Int, b: Int): Int =
     if (a == b && a == 0) 0 else Math.abs(a * b) / gcd(a, b)
 
--- src/test/scala/HomeworkTest.scala
+++ src/test/scala/HomeworkTest.scala
@@ -1,18 +1,18 @@
-import evo.homework.Main
+import evo.homework.Basics
 import org.scalatest.FunSuite
 
 class HomeworkTest extends FunSuite {
   test ("Main.gcd"){
-    assert(Main.gcd(3,5) == 1)
-    assert(Main.gcd(0,5) == 5)
-    assert(Main.gcd(24,12) == 12)
-    assert(Main.gcd(0,0) == 0)
+    assert(Basics.gcd(3,5) == 1)
+    assert(Basics.gcd(0,5) == 5)
+    assert(Basics.gcd(24,12) == 12)
+    assert(Basics.gcd(0,0) == 0)
   }
 
   test ("Main.lcm"){
-    assert(Main.lcm(3,0) == 0)
-    assert(Main.lcm(3,5) == 15)
-    assert(Main.lcm(0,0) == 0)
-    assert(Main.lcm(1,5) == 5)
+    assert(Basics.lcm(3,0) == 0)
+    assert(Basics.lcm(3,5) == 15)
+    assert(Basics.lcm(0,0) == 0)
+    assert(Basics.lcm(1,5) == 5)
   }
 }
