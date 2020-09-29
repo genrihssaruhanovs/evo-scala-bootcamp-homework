1f2caa1f9928521ad09345b5341371fa0d5c346d
Genrihs Saruhanovs <genrihs.saruhanovs@gmail.com>
2020-09-29 13:06:50 +0300

    Second homework implementation

--- src/test/scala/BasicsTest.scala
+++ src/test/scala/BasicsTest.scala
@@ -1,15 +1,15 @@
 import evo.homework.Basics
 import org.scalatest.FunSuite
 
-class HomeworkTest extends FunSuite {
-  test ("Main.gcd"){
+class BasicsTest extends FunSuite {
+  test ("Basics.gcd"){
     assert(Basics.gcd(3,5) == 1)
     assert(Basics.gcd(0,5) == 5)
     assert(Basics.gcd(24,12) == 12)
     assert(Basics.gcd(0,0) == 0)
   }
 
-  test ("Main.lcm"){
+  test ("Basics.lcm"){
     assert(Basics.lcm(3,0) == 0)
     assert(Basics.lcm(3,5) == 15)
     assert(Basics.lcm(0,0) == 0)
