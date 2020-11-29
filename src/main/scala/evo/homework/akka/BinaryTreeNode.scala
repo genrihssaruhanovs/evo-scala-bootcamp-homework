3a50c129fe691abdeb68d61366147e0c8034faf5
genrihssaruhanovs <genrihs.saruhanovs@gmail.com>
2020-11-29 20:00:25 +0200

    Akka homework implementation

--- src/main/scala/evo/homework/akka/BinaryTreeNode.scala
+++ src/main/scala/evo/homework/akka/BinaryTreeNode.scala
@@ -3,7 +3,7 @@ package evo.homework.akka
 import akka.actor.{Actor, ActorRef, Props}
 
 object BinaryTreeNode {
-  private sealed trait Position
+  sealed private trait Position
 
   private case object Left extends Position
   private case object Right extends Position
@@ -20,18 +20,47 @@ final class BinaryTreeNode(val elem: Int, initiallyRemoved: Boolean) extends Act
   private var removed = initiallyRemoved
 
   override def receive: Receive = {
-    case _ => ???
+    case i @ Insert(_, _, _)   => doInsert(i)
+    case c @ Contains(_, _, _) => doContains(c)
+    case r @ Remove(_, _, _)   => doRemove(r)
   }
 
   private def doInsert(m: Insert): Unit = {
-    ???
+    if (m.elem == elem) m.requester ! OperationFinished(m.id)
+    else {
+      val position = if (m.elem > elem) Right else Left
+      subtrees.get(position) match {
+        case Some(childActor) => childActor ! m
+        case None =>
+          subtrees += (position -> context.actorOf(BinaryTreeNode.props(m.elem, initiallyRemoved = false)))
+          m.requester ! OperationFinished(m.id)
+      }
+    }
   }
 
   private def doContains(m: Contains): Unit = {
-    ???
+    if (m.elem == elem && !removed) m.requester ! ContainsResult(m.id, result = true)
+    else {
+      val position = if (m.elem > elem) Right else Left
+      subtrees.get(position) match {
+        case Some(childActor) => childActor ! m
+        case None =>
+          m.requester ! ContainsResult(m.id, result = false)
+      }
+    }
   }
 
   private def doRemove(m: Remove): Unit = {
-    ???
+    if (m.elem == elem) {
+      removed = true
+      m.requester ! OperationFinished(m.id)
+    } else {
+      val position = if (m.elem > elem) Right else Left
+      subtrees.get(position) match {
+        case Some(childActor) => childActor ! m
+        case None =>
+          m.requester ! OperationFinished(m.id)
+      }
+    }
   }
 }
